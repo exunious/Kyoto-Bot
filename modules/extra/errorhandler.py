abf3d8497acc8c9abfdd3171aa46374f1ba52959
Ashleeyy <ashley.muis@gmail.com>
2017-10-07 08:39:02 +0200

    Error Handler Fixed

--- modules/extra/errorhandler.py
+++ modules/extra/errorhandler.py
@@ -14,7 +14,10 @@ class ErrorHandler:
 				embed = discord.Embed(title = "Command not found!", description = "**" + ctx.author.name + "** for a list with all my commands please use: `-help` or `-h`", color = 13434880)
 				embed.add_field(name="Command Used: ", value = ctx.message.content)
 				await ctx.send(embed = embed)
+			try:
 				await ctx.message.delete()
+			except:
+				pass
 
 				if isinstance (error, commands.BotMissingPermissions):
 					pass
